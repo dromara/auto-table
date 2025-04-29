c7b1753e4d845ed100f1b6aa4cad7da8de5bf915
chengliang4810 <chengliang4810@163.com>
2025-04-30 00:40:47 +0800

    feat(datasource): 增强Solon数据源处理器的动态数据源支持
    改进SolonDataSourceHandler以更好地处理动态数据源场景：
    1. 添加对静态数据源的直接支持
    2. 增加动态数据源查找逻辑
    3. 添加异常处理和日志警告
    4. 支持通过名称获取特定数据源
    5. 优化数据源管理逻辑

--- auto-table-solon-plugin/src/main/java/org/dromara/autotable/solon/adapter/SolonDataSourceHandler.java
+++ auto-table-solon-plugin/src/main/java/org/dromara/autotable/solon/adapter/SolonDataSourceHandler.java
@@ -1,32 +1,67 @@
 package org.dromara.autotable.solon.adapter;
 
+import cn.hutool.core.lang.Assert;
 import cn.hutool.core.util.StrUtil;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.dromara.autotable.core.dynamicds.DataSourceManager;
 import org.dromara.autotable.core.dynamicds.IDataSourceHandler;
+import org.dromara.autotable.solon.exception.DataSourceNotFoundException;
 import org.noear.solon.Solon;
 import org.noear.solon.annotation.Component;
 import org.noear.solon.core.BeanWrap;
+import org.noear.solon.data.dynamicds.DynamicDataSource;
 import org.noear.solon.data.dynamicds.DynamicDs;
 import org.noear.solon.data.dynamicds.DynamicDsKey;
 
 import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Solon数据源处理器
  *
  * @author chengliang
- * @date 2025/01/08
  */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class SolonDataSourceHandler implements IDataSourceHandler {
 
     @Override
     public void useDataSource(String dataSourceName) {
-        DynamicDsKey.use(dataSourceName);
-        DataSource dataSource = Solon.context().getWrap(DataSource.class).get();
+
+        // 获取框架中的所有数据源
+        Map<String, DataSource> dataSourceMap = Solon.context().getBeansMapOfType(DataSource.class);
+
+        // 根据数据源名称获取数据源，如果存在的话，直接使用。
+        DataSource staticDataSource = dataSourceMap.get(dataSourceName);
+        if (staticDataSource != null) {
+            DataSourceManager.setDataSource(staticDataSource);
+            return;
+        }
+
+        // 获取动态数据源列表
+        List<DataSource> dynamicDataSourceList = dataSourceMap.values().stream()
+                .filter(ds -> ds instanceof DynamicDataSource).collect(Collectors.toList());
+
+        Assert.notEmpty(dynamicDataSourceList,() -> new DataSourceNotFoundException("未找到数据源"));
+
+        if (dynamicDataSourceList.size() != 1){
+            log.warn("项目中存在多个动态数据源，仅使用第一个动态数据源。");
+        }
+
+        // 找到第一个动态数据源
+        DynamicDataSource dynamicDataSource = (DynamicDataSource) dynamicDataSourceList.get(0);
+
+        // 获取内部数据源
+        DataSource dataSource = dynamicDataSource.getDefaultTargetDataSource();
+        if (StrUtil.isNotBlank(dataSourceName)){
+            dataSource = dynamicDataSource.getTargetDataSource(dataSourceName);
+        }
+
         DataSourceManager.setDataSource(dataSource);
     }
 
