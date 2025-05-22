1ce95320007202355abe136091b65f5dd777f55c
tangzc <imtzc@qq.com>
2025-05-22 22:52:53 +0800

    元数据适配器返回值优化

--- auto-table-core/src/main/java/org/dromara/autotable/core/AutoTableMetadataAdapter.java
+++ auto-table-core/src/main/java/org/dromara/autotable/core/AutoTableMetadataAdapter.java
@@ -114,7 +114,7 @@ public interface AutoTableMetadataAdapter {
      * @param clazz 类
      * @return 是否忽略
      */
-    default boolean isIgnoreField(Field field, Class<?> clazz) {
+    default Boolean isIgnoreField(Field field, Class<?> clazz) {
         // 默认所有字段均不被排除
         return false;
     }
@@ -126,7 +126,7 @@ public interface AutoTableMetadataAdapter {
      * @param clazz 类
      * @return 是否是主键
      */
-    default boolean isPrimary(Field field, Class<?> clazz) {
+    default Boolean isPrimary(Field field, Class<?> clazz) {
         // 默认不是主键
         return false;
     }
@@ -138,7 +138,7 @@ public interface AutoTableMetadataAdapter {
      * @param clazz 类
      * @return 是否是自增的主键
      */
-    default boolean isAutoIncrement(Field field, Class<?> clazz) {
+    default Boolean isAutoIncrement(Field field, Class<?> clazz) {
         // 默认都不是自增的
         return false;
     }
@@ -150,7 +150,7 @@ public interface AutoTableMetadataAdapter {
      * @param clazz 实体类
      * @return 字段是否非空
      */
-    default boolean isNotNull(Field field, Class<?> clazz) {
+    default Boolean isNotNull(Field field, Class<?> clazz) {
         return false;
     }
 }
