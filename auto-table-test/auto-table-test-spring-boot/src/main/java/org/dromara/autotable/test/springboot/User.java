c6a13b41128362b34babb72b5e64ac95cc9aba43
lizhian <503457938@qq.com>
2025-05-30 16:12:20 +0800

    feat(test): 添加 Oracle 数据库支持并调整测试配置
    - 新增 Oracle 数据库配置文件 application-oracle.yml
    - 添加 OracleUser 类以测试 Oracle 数据库功能
    - 修改 User 类，注释掉部分字段以适应 Oracle 数据库
    - 关闭 SQL 记录功能
    - 设置 Oracle 数据库为默认配置

--- auto-table-test/auto-table-test-spring-boot/src/main/java/org/dromara/autotable/test/springboot/User.java
+++ auto-table-test/auto-table-test-spring-boot/src/main/java/org/dromara/autotable/test/springboot/User.java
@@ -1,11 +1,9 @@
 package org.dromara.autotable.test.springboot;
 
+import lombok.Data;
 import org.dromara.autotable.annotation.AutoTable;
 import org.dromara.autotable.annotation.ColumnComment;
-import org.dromara.autotable.annotation.ColumnType;
 import org.dromara.autotable.annotation.PrimaryKey;
-import org.dromara.autotable.annotation.mysql.MysqlTypeConstant;
-import lombok.Data;
 
 @Data
 @AutoTable("sys_user")
@@ -22,6 +20,6 @@ public class User {
     private Integer age;
 
     @ColumnComment("备注")
-    @ColumnType(MysqlTypeConstant.TEXT)
+    //@ColumnType(MysqlTypeConstant.TEXT)
     private String mark;
 }
