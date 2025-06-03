d12aba0e28bad296e4cd52d0c666fc1fd8bdc0fc
lizhian <503457938@qq.com>
2025-06-03 22:52:03 +0800

    feat(auto-table): 更新配置并添加 Oracle 数据库支持
    - 修改 application.yml 配置，启用 SQL记录并添加 Oracle 11g 配置
    - 新增 OracleUser 类，支持 Oracle 数据库表结构
    - 更新 User 类，增加对 Oracle 数据库的兼容性
    - 添加 Oracle 相关注解和策略导入，以支持 Oracle 数据库特性

--- auto-table-test/auto-table-test-spring-boot/src/main/java/org/dromara/autotable/test/springboot/OracleUser.java
+++ auto-table-test/auto-table-test-spring-boot/src/main/java/org/dromara/autotable/test/springboot/OracleUser.java
@@ -10,30 +10,32 @@ import java.util.Date;
 @Data
 @AutoTable(value = "oracle_user", comment = "修改注释")
 @TableIndex(type = IndexTypeEnum.UNIQUE, indexFields = {
-        @IndexField(field = "age", sort = IndexSortTypeEnum.DESC)
-        , @IndexField(field = "phone", sort = IndexSortTypeEnum.DESC)
+        @IndexField(field = "phone", sort = IndexSortTypeEnum.DESC)
+        , @IndexField(field = "age", sort = IndexSortTypeEnum.DESC)
+
 })
 public class OracleUser {
+
+    @PrimaryKey(autoIncrement = true)
     @ColumnComment("用户id")
     private Long id;
 
-    // @PrimaryKey(autoIncrement = true)
     @ColumnComment("用户id2")
     private Long id2;
 
     @AutoColumn(value = "PHONe", notNull = true)
     @ColumnComment("电话")
-    @ColumnDefault("17")
-    @ColumnType(value = "varchar2", length = 1999)
+    @ColumnDefault("50")
+    @ColumnType(value = "varchar2", length = 300)
     private String phone;
 
     @ColumnComment("年龄2")
     @ColumnDefault("11")
     private Integer age;
 
-    /*@ColumnComment("备注2")
+    @ColumnComment("备注2")
     @ColumnType("CLOB")
-    private String mark;*/
+    private String mark;
 
 
     @AutoColumn(comment = "创建时间", defaultValue = "SYSDATE")
