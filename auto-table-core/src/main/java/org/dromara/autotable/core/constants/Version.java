63a7fb7824383bb52e266ae3a26975db305a999b
tangzc <imtzc@qq.com>
2026-09-04 13:25:00 +0800

    版本升级：2.7.0

--- auto-table-core/src/main/java/org/dromara/autotable/core/constants/Version.java
+++ auto-table-core/src/main/java/org/dromara/autotable/core/constants/Version.java
@@ -1,1 +1,1 @@
-package org.dromara.autotable.core.constants; public interface Version { String VALUE = "2.6.5"; }
+package org.dromara.autotable.core.constants; public interface Version { String VALUE = "2.7.0"; }
